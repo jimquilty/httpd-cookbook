1ee6f9abe75db44b54c17e07f3b0cdd145cd173a
Jim Quilty <jquilty@chef.io>
2020-07-22 14:20:38 -0400

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# httpd-cookbook
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: httpd-cookbook
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
